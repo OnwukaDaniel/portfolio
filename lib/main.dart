65dfca3291e11ca1ad543b6e152efb191697a93d
Onwuka Daniel <onwukadaniel16@gmail.com>
2023-11-26 19:13:59 +0100

    Set up for windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,18 @@
-import 'dart:async';
-
 import 'package:portfolio/imports/common_imports.dart';
 import 'package:portfolio/ui/homepage/views/homepage_view.dart';
-
 import 'globals/theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-
+      title: 'My Portfolio',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const MyHomePage(),
+      home: const HomepageView(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (_) {
-        return const HomepageView();
-      }), (route) => false);
-    });
-
-    return Image.asset("assets/home/code_image_large.jpg", fit: BoxFit.cover);
   }
 }
