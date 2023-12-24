03128b19b72c27b6f026012f9005523a714035c0
Onwuka Daniel <onwukadaniel16@gmail.com>
2023-12-24 21:25:15 +0100

    import resolve

--- lib/imports/common_imports.dart
+++ lib/imports/common_imports.dart
@@ -1,10 +1,6 @@
 export 'dart:async';
 export 'package:flutter/material.dart';
 export 'package:portfolio/globals/colorUtils.dart';
-export 'package:portfolio/globals/gap.dart';
-export 'package:portfolio/globals/strings.dart';
-export 'package:portfolio/ui/homepage/viewmodel/homepage_viewmodel.dart';
-export 'package:portfolio/ui/homepage/enums/appbar_action_enum.dart';
 export 'package:stacked/stacked.dart';
 export 'package:stacked_hooks/stacked_hooks.dart';
 export 'package:portfolio/ui/homepage/widgets/widgets.dart';
@@ -12,3 +8,7 @@ export 'package:portfolio/globals/theme.dart';
 export 'package:portfolio/globals/textUtils.dart';
 export 'package:portfolio/globals/appColor.dart';
 export 'package:portfolio/ui/homepage/views/homepage_view.dart';
+export 'package:portfolio/globals/gap.dart';
+export 'package:portfolio/globals/strings.dart';
+export 'package:portfolio/ui/homepage/viewmodel/homepage_viewmodel.dart';
+export 'package:portfolio/ui/homepage/enums/appbar_action_enum.dart';
