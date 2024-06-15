70a8e073170646f0db607f522841f7300d4ebabd
Onwuka Daniel <onwukadaniel16@gmail.com>
2024-06-15 23:36:44 +0100

    Edit bio data to get larger paragraph

--- lib/ui/about_me/repo/about_me_repo.dart
+++ lib/ui/about_me/repo/about_me_repo.dart
@@ -10,24 +10,24 @@ class AboutMeRepo {
           AboutSideBarInfo(
             file: 'bio.dart',
             info: '/**'
-                '\n**     About Me\n* With 4 years of experience in software development, '
-                '\n** I am a results-oriented IT professional dedicated to delivering '
-                '\n** high-quality solutions. My excellent communication skills and passion'
-                '\n** for problem-solving enable me to effectively collaborate with team '
-                '\n** members and stakeholders, ensuring project success. '
-                '\n**     I am proficient in version control and adept at working with '
-                '\n** third-party APIs to enhance functionality. My expertise includes '
-                '\n** UI/UX design, where I strive to create user-centric solutions that '
-                '\n** meet the needs and expectations of end-users. Additionally, I have a'
-                '\n** strong background in research, Firebase, and Google Maps integration,'
-                '\n** which allows me to develop robust and scalable applications. My technical'
-                '\n** skill set includes a deep understanding of RESTful API integration,'
-                '\n** state management, and the implementation of CI/CD practices for continuous'
-                '\n** integration and continuous development. I excel in debugging processes,'
-                '\n** ensuring that issues are resolved promptly to maintain the quality and'
-                '\n** performance of the software.\n * I am eager to bring my diverse skill set'
-                '\n** to a dynamic and collaborative tech environment where I can continue to grow'
-                '\n** and contribute to innovative projects.\n**/',
+                '\n**       About Me:\n* With 4 years of experience in software development, '
+                '\n**   I am a results-oriented IT professional dedicated to delivering '
+                '\n**   high-quality solutions. My excellent communication skills and passion'
+                '\n**   for problem-solving enable me to effectively collaborate with team '
+                '\n**   members and stakeholders, ensuring project success. '
+                '\n**       I am proficient in version control and adept at working with '
+                '\n**   third-party APIs to enhance functionality. My expertise includes '
+                '\n**   UI/UX design, where I strive to create user-centric solutions that '
+                '\n**   meet the needs and expectations of end-users. Additionally, I have a'
+                '\n**   strong background in research, Firebase, and Google Maps integration,'
+                '\n**   which allows me to develop robust and scalable applications. My technical'
+                '\n**   skill set includes a deep understanding of RESTful API integration,'
+                '\n**   state management, and the implementation of CI/CD practices for continuous'
+                '\n**   integration and continuous development. I excel in debugging processes,'
+                '\n**   ensuring that issues are resolved promptly to maintain the quality and'
+                '\n**   performance of the software.\n * I am eager to bring my diverse skill set'
+                '\n**   to a dynamic and collaborative tech environment where I can continue to grow'
+                '\n**   and contribute to innovative projects.\n**/',
           )
         ],
         show: true,
