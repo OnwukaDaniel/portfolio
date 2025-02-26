f32f9bdffc328c0f869f220ce6bf9d2a4ba68196
OnwukaDaniel <OnwukaDaniel@users.noreply.github.com>
2025-02-26 20:50:38 +0000

    deploy: 13b712f0651c1d99757f20e9925900f90e5fc510

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4079340915"
+    serviceWorkerVersion: "1805836003"
   }
 });
